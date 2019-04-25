89b2be4b5cf41a7e9a62d8d98dd430fb9652c52b
Vadim Geroim <vgeroim.qa@gmail.com>
2019-04-24 21:01:12 -0400

    run migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :events
 
-  validate :name, presence: true, length: {maximum: 35}
-  validate :email, presence: true, length: {maximum: 255}, uniqueness: true, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
+  validates :name, presence: true, length: {maximum: 35}
+  validates :email, presence: true, length: {maximum: 255}, uniqueness: true, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
 end
