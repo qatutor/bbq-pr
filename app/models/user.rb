2c929f3024068fd5bfc7f3ebaab099438c50eb4a
Vadim Geroim <vgeroim.qa@gmail.com>
2019-04-29 16:48:32 -0400

    run migration for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :events
-
+  before_validation :set_name, on: :create
   validates :name, presence: true, length: {maximum: 35}
-  validates :email, presence: true, length: {maximum: 255}, uniqueness: true, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
+
+  private
+  def set_name
+    self.name = "User #{rand(1000)}" if self.name.blank?
+  end
 end
