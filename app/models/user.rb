776bcc702646d7de4b573214dfac121ed4949991
Vadim Geroim <vgeroim.qa@gmail.com>
2019-05-02 16:37:32 -0400

    replace to localization messages in user and event views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :events
-  before_validation :set_name, on: :create
   validates :name, presence: true, length: {maximum: 35}
-
-  private
-  def set_name
-    self.name = "User #{rand(1000)}" if self.name.blank?
-  end
 end
