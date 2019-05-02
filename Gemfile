7d94814d80d8648e233d360a0e1cc4934327b9ef
Vadim Geroim <vgeroim.qa@gmail.com>
2019-05-02 08:51:56 -0400

    alert devise errors

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.6.0'
 gem 'rails', '~> 5.2.3'
 gem "twitter-bootstrap-rails"
 gem 'devise'
+gem 'jquery-rails'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
 //= require rails-ujs
+//= require jquery3
+//= require twitter/bootstrap/alert
 //= require activestorage
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
