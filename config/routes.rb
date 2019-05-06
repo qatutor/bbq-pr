95f177fb0129716199d33dd199298c954fde7769
Vadim Geroim <vgeroim.qa@gmail.com>
2019-05-06 11:59:07 -0400

    add comments to routes and create comments form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'events#index'
-  resources :events
+  resources :events do
+    resources :comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 end
