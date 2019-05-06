aaa338835426dd6424f247cad66c7a4ae14b0355
Vadim Geroim <vgeroim.qa@gmail.com>
2019-05-06 15:41:49 -0400

    add validation on user present to model subscription

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :subscriptions
 
   validates :user, presence: true
   validates :title, presence: true, length: {maximum: 255}
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :events
   has_many :comments
+  has_many :subscriptions
   validates :name, presence: true, length: {maximum: 35}
 end
