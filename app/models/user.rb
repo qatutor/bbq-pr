360e16fb5f15a9ef0f9582bfd37eb8dde4da89d2
Vadim Geroim <vgeroim.qa@gmail.com>
2019-05-06 10:39:47 -0400

    add Comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :events
+  has_many :comments
   validates :name, presence: true, length: {maximum: 35}
 end
